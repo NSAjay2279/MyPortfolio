eda1d08edd3ac18fe4a0584e24152438806977b0
Ajay Singh Negi <negisinghajayus@gmail.com>
2023-11-11 18:07:03 +0530

    Files Updated

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var Pool = require('pg').Pool;
+
+var config = {
+	user: 'postgres',
+	database: 'portdb',
+	host: 'localhost',
+	port: '5432',
+	password: 'PostGres'
+};
 
 var app = express();
 app.use(morgan('combined'));
@@ -72,7 +81,7 @@ function createTemplate (data) {
 					${heading}
 				</h3>
 				<div>
-					${date}
+					${date.toDateString()}
 				</div>
 				<div>
 					${content}
@@ -88,19 +97,53 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/:articleName', function (req, res) {
-	var articleName = req.params.articleName;
-	res.send(createTemplate(articles[articleName]));
+var pool = new Pool(config);
+app.get('/test-db', function(req, res) {
+	pool.query('SELECT * FROM test', function(err, result) {
+		if(err) {
+			res.status(500).send(err.toString());
+		} else {
+			res.send(JSON.stringify(result.rows));
+		}
+	});
+});
+
+var counter = 0;
+app.get('/counter', function (req, res) {
+	counter = counter + 1;
+	res.send(counter.toString());
+});
+
+var names = [];
+app.get('/submit-name', function (req, res) {
+  var name = req.query.name ;
+  names.push(name);
+  res.send(JSON.stringify(names));
+});
+
+app.get('/articles/:articleName', function (req, res) {
+	
+	pool.query("SELECT * FROM article WHERE title = $1", [req.params.articleName], function(err, result){
+		if(err) {
+			res.status(500).send(err.toString());
+		} else {
+			if (result.rows.length === 0) {
+				res.status(400).send('Article not found');
+			} else {
+				var articleData = result.rows[0];
+				res.send(createTemplate(articleData));
+			}
+		}
+	});
 });
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
-app.get('/ui/madi.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
+app.get('/ui/main.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
-
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,46 @@
-console.log('Loaded!');
+var button = document.getElementById('counter');
+
+button.onclick = function() {
+
+  var request = new XMLHttpRequest();
+
+  request.onload = function() {
+    if (request.status === 200) {
+      var counter = request.responseText;
+      var span = document.getElementById('count');
+      span.innerHTML = counter.toString();
+    } else {
+      // Handle error
+    }
+  };
+
+  request.open('GET', 'http://localhost:8080/counter', true);
+  request.send(null);
+};
+
+var submit = document.getElementById('submit_btn');
+
+submit.onclick = function() {
+
+  var request = new XMLHttpRequest();
+
+  request.onload = function() {
+    if (request.status === 200) {
+      var names = request.responseText;
+      names = JSON.parse(names);
+      var list = '';
+      for (var i = 0; i < names.length; i++) {
+        list += '<li>' + names[i] + '</li>';
+      }
+      var ul = document.getElementById('namelist');
+      ul.innerHTML = list;
+    } else {
+      // Handle error
+    }
+  };
+
+  var nameInput = document.getElementById('name');
+  var name = nameInput.value;
+  request.open('GET', 'http://localhost:8080/submit-name?name=' + name, true);
+  request.send(null);
+};
